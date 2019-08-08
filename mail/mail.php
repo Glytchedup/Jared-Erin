b97127dd5d44db0c23e14087fec9ecedd0e91d52
Farhan Rahman <arfarhan44@gmail.com>
2019-08-09 03:41:27 +0600

    New

--- mail/mail.php
+++ mail/mail.php
@@ -11,8 +11,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $error = array();
 
     if (empty($error)) {
-        $to = "arfarhan44@gmail.com";
+        $to = "davekubo@gmail.com";
         $subject = "RSVP Data";
+
         // Always set content-type when sending HTML email
         $headers = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
