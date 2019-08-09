47d31c256e34e9bac2a65a3b91ab2e9bc629e63b
David <davekubo@gmail.com>
2019-08-08 23:45:19 -0700

    Changed Color Scheme

--- index.php
+++ index.php
@@ -132,8 +132,10 @@
                             Rittenhouse Rd, <br /> Schnepf Farms, <br />Queen Creek, <br />
                             <i class="flaticon-map35"></i></a></div>
                             <br />
-                    <div class="title3"><a class="maplink" href="https://youtu.be/Ujmwr95u9ro" target="_blank">
+                        <div>
+                            <div class="title3"><a class="maplink" href="https://youtu.be/Ujmwr95u9ro" target="_blank">
                             Click here for video directions <br /> from Cloud Rd entrance </div>
+                        </div>
               
                     <div class="pattern2"></div>
                     <div class="title1">10 thirty</div>
