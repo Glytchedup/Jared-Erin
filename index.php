8c3b5b4dceca2598d3dbccf09f8b67dafdc98880
David <davekubo@gmail.com>
2019-08-08 21:25:42 -0700

    small pic tweaks

--- index.php
+++ index.php
@@ -150,10 +150,6 @@
             <!-- Wrapper -->
             <div class="story_wrapper">
 
-var str = "Amazon Wedding";
-var result = str.link("https://www.amazon.com/wedding/share/ErAndJarBear");
-
-
 <!-- Item -->
 <div class="story_item">
     <div class="story_img parallax" data-image="img/Registry.jpg">
@@ -169,7 +165,7 @@ var result = str.link("https://www.amazon.com/wedding/share/ErAndJarBear");
         <!-- Registry Links -->
         <div class="title3"><a class="maplink" href="https://www.amazon.com/wedding/share/ErAndJarBear" target="_blank">
                                     Erin and Jared <br /> are registered on <br />Amazon Wedding <br />
-                                    <i class="flaticon-map35"></i></a></div>
+                                    <i class="flaticon-diamond30"></i></a></div>
                         </div>
                     </div>
                 </div>
